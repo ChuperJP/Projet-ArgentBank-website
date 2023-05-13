22340428c2fdf9f5ff1d31b2478a183e261ca7c0
chuper <chuper@gmx.fr>
2023-05-13 23:27:43 +0200

    #

--- FrontEnd/argent_bank/src/components/UserHeader/index.jsx
+++ FrontEnd/argent_bank/src/components/UserHeader/index.jsx
@@ -12,12 +12,18 @@ function UserHeader() {
   const dispatch = useDispatch()
   const firstName = userInfo.firstName
   const lastName = userInfo.lastName
-
+  // const displayFormEdit = () => {
+  //   if(editInfo.open === true){
+  //     return <FormEditInfo />
+  //   }
+  //   return null
+  // }
   return (
     <div className="header">
         {editInfo.open === true ? <h1>Edit user info</h1> : <h1>Welcome back<br />{firstName} {lastName}!</h1>}
         {editInfo.open === true ? null : <button className="edit-button" onClick={()=> dispatch(toggleEdit())}>Edit Name</button>}
         {editInfo.open === true ?<FormEditInfo /> : null}
+        {/* {displayFormEdit()} */}
       </div>
   )
 }
