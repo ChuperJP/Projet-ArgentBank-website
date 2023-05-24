f0c4480d87a4d3c4246d2395d47b94afa23b20f9
chuper <chuper@gmx.fr>
2023-05-24 13:37:47 +0200

    #

--- FrontEnd/argent_bank/src/components/FormEditInfo/index.jsx
+++ FrontEnd/argent_bank/src/components/FormEditInfo/index.jsx
@@ -1,70 +1,81 @@
-import React from 'react'
- import '../../style/FormEditInfo/formEditInfo.css'
-import { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { selectUserInfo, selectLog } from '../../utils/selectors';
-import { changeUserName } from '../../features/userInfo.js'
-import { toggleEdit } from '../../features/editInfo.js'
+import React from "react";
+import "../../style/FormEditInfo/formEditInfo.css";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUserInfo, selectLog } from "../../utils/selectors";
+import { changeUserName } from "../../features/userInfo.js";
+import { toggleEdit } from "../../features/editInfo.js";
 
 function FormEditInfo() {
-    const [userName, setUserName] = useState("")
-    const dispatch = useDispatch()
-    const log = useSelector(selectLog)
-    const userInfo = useSelector(selectUserInfo)
-    const firstName = userInfo.firstName
-    const lastName = userInfo.lastName
+  const [userName, setUserName] = useState("");
+  const dispatch = useDispatch();
+  const log = useSelector(selectLog);
+  const userInfo = useSelector(selectUserInfo);
+  const firstName = userInfo.firstName;
+  const lastName = userInfo.lastName;
 
+  const editPost = async () => {
+    const logResponse = await fetch(
+      "http://localhost:3001/api/v1/user/profile",
+      {
+        method: "PUT",
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + log.token,
+        },
+        body: JSON.stringify({
+          userName: userName,
+        }),
+      }
+    );
+    const editData = await logResponse.json();
+    dispatch(changeUserName(editData.body.userName));
+  };
 
-    const editPost = async() => {
-        const logResponse = await fetch ('http://localhost:3001/api/v1/user/profile',{
-          method: 'PUT',
-          headers: {
-            'accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': "Bearer " + log.token
-          },
-          body: JSON.stringify({
-            userName: userName
-          }),
-        });
-        const editData = await logResponse.json();
-        dispatch(changeUserName(editData.body.userName))
-    }
-
-
-    const handleSubmit = async(e) => {
-      e.preventDefault()
-      editPost()
-      setUserName("")
-    }
-    const handleClick = async(e) => {
-      e.preventDefault()
-      dispatch(toggleEdit())
-      setUserName("")
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    editPost();
+    setUserName("");
+  };
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch(toggleEdit());
+    setUserName("");
+  };
 
   return (
-    <div className='editForm_wrapper'>
-        <form type="submit" onSubmit={handleSubmit}>
-          <div className="editInfo_input-wrapper">
-            <label htmlFor="username">User name:</label>
-            <input type="text" id="username" value={userName} onChange={(e)=>setUserName(e.target.value)} required/>
-          </div>
-          <div className="editInfo_input-wrapper">
-            <label htmlFor="firstName">First name:</label>
-            <input type="firstName" id="firstName" value={firstName} disabled/>
-          </div>
-          <div className="editInfo_input-wrapper">
-            <label htmlFor="lastName">Last name:</label>
-            <input type="lastName" id="lastName" value={lastName} disabled/>
-          </div>
-          <div className='button_wrapper'>
-            <button className="sign-in-button" type="submit">Save</button>
-            <button className="sign-in-button" onClick={handleClick}>Cancel</button> 
-          </div>
-        </form>
+    <div className="editForm_wrapper">
+      <form type="submit" onSubmit={handleSubmit}>
+        <div className="editInfo_input-wrapper">
+          <label htmlFor="username">User name:</label>
+          <input
+            type="text"
+            id="username"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="editInfo_input-wrapper">
+          <label htmlFor="firstName">First name:</label>
+          <input type="firstName" id="firstName" value={firstName} disabled />
+        </div>
+        <div className="editInfo_input-wrapper">
+          <label htmlFor="lastName">Last name:</label>
+          <input type="lastName" id="lastName" value={lastName} disabled />
+        </div>
+        <div className="button_wrapper">
+          <button className="sign-in-button" type="submit">
+            Save
+          </button>
+          <button className="sign-in-button" onClick={handleClick}>
+            Cancel
+          </button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default FormEditInfo
+export default FormEditInfo;
--- FrontEnd/argent_bank/src/components/Header/index.jsx
+++ FrontEnd/argent_bank/src/components/Header/index.jsx
@@ -1,46 +1,43 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import "../../style/Header/header.css"
-import LogoBank from "../../assets/argentBankLogo.webp"
-import { useDispatch, useSelector } from "react-redux"
-import { selectLog, selectUserInfo, selectEditInfo } from '../../utils/selectors';
-import { logOut } from '../../features/log';
-import { removeUserInfo } from '../../features/userInfo'
-import { toggleEdit } from "../../features/editInfo"
-
-
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "../../style/Header/header.css";
+import LogoBank from "../../assets/argentBankLogo1.webp";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectLog,
+  selectUserInfo,
+  selectEditInfo,
+} from "../../utils/selectors";
+import { logOut } from "../../features/log";
+import { removeUserInfo } from "../../features/userInfo";
+import { toggleEdit } from "../../features/editInfo";
 
 function Header() {
-
-  
-  const dispatch = useDispatch()
-  const log = useSelector(selectLog)
-  const userInfo = useSelector(selectUserInfo)
-  const editInfo = useSelector(selectEditInfo)
-  const pseudo = userInfo.userName
+  const dispatch = useDispatch();
+  const log = useSelector(selectLog);
+  const userInfo = useSelector(selectUserInfo);
+  const editInfo = useSelector(selectEditInfo);
+  const pseudo = userInfo.userName;
 
   const isConnected = () => {
-    if (log.token === null){
-      return false
+    if (log.token === null) {
+      return false;
     }
-      return true
-  }
+    return true;
+  };
 
-
-  const signOut = () =>{
-    dispatch(removeUserInfo())
-    if(editInfo.open === true){
-      dispatch(toggleEdit())
+  const signOut = () => {
+    dispatch(removeUserInfo());
+    if (editInfo.open === true) {
+      dispatch(toggleEdit());
     }
-    dispatch(logOut())
-  }
+    dispatch(logOut());
+  };
 
-  const handleClick = (e) =>{
-    signOut()
-  }
-  
- 
+  const handleClick = (e) => {
+    signOut();
+  };
+
   return (
     <nav className="main-nav">
       <NavLink className="main-nav-logo" to="/">
@@ -52,27 +49,27 @@ function Header() {
         <h1 className="sr-only">Argent Bank</h1>
       </NavLink>
       <div>
-        {isConnected() === true ? null :
+        {isConnected() === true ? null : (
           <NavLink className="main-nav-item" to="/sign-in">
-          <i className="fa fa-user-circle"></i>
-          Sign In
+            <i className="fa fa-user-circle"></i>
+            Sign In
           </NavLink>
-          }
-        { isConnected() === false ? null :
+        )}
+        {isConnected() === false ? null : (
           <NavLink className="main-nav-item" to="/user">
-          <i className="fa fa-user-circle"></i>
-          {pseudo}
-          </NavLink> 
-        }
-        { isConnected() === false ? null :
+            <i className="fa fa-user-circle"></i>
+            {pseudo}
+          </NavLink>
+        )}
+        {isConnected() === false ? null : (
           <NavLink className="main-nav-item" to="/" onClick={handleClick}>
-          <i className="fa fa-sign-out"></i>
-          Sign Out
-          </NavLink> 
-          }
+            <i className="fa fa-sign-out"></i>
+            Sign Out
+          </NavLink>
+        )}
       </div>
     </nav>
-  )
+  );
 }
 
-export default Header
+export default Header;
